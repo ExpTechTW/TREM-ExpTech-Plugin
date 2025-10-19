81b4c170d7085e80c300b2538818ef3063031dc2
夜夜 <vcde0000@gmail.com>
2025-10-19 22:07:17 +0800

    build: 1.1.0

--- src/exptech.js
+++ src/exptech.js
@@ -24,24 +24,67 @@ class ExpTech {
     const timeout = setTimeout(() => controller.abort(), 60000);
 
     try {
-      const res = await fetch("https://api-1.exptech.dev/api/v3/et/login", {
-        method  : "POST",
-        signal  : controller.signal,
-        headers : { "Content-Type": "application/json" },
-        body    : JSON.stringify({
-          email : this.getconfig.user.email,
-          pass  : this.getconfig.user.pass,
-          name  : `${this.getconfig.user.name}/TREM-lite/3.1.0-rc.4/${os.release()}`
+      const response = await fetch('https://api.exptech.dev/api/v1/auth/login', {
+        body: JSON.stringify({
+          email    : this.getconfig.user.email,
+          password : this.getconfig.user.pass
         }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        method: 'POST',
       });
 
-      if (res && res.ok) {
-        const ans = await res.text();
-        this.key = ans;
-        this.getconfig.user.token = this.key
+      if (response && response.ok) {
+        const ans = await response.json();
+        // this.logger.info("Login: ", ans);
+        this.login_token = ans.token;
+        this.refresh_login_token = ans.refresh_token;
+        this.expires_at = ans.expires_at;
+        this.getconfig.login.token = this.login_token;
+        this.getconfig.login.refresh_token = this.refresh_login_token;
+        this.getconfig.login.expires_at = this.expires_at;
         this.config.writeConfig(this.getconfig);
+        return await this.#serviceToken();
       } else
-        this.logger.error("Login http status code: ", res.status);
+        this.logger.error("Login http status code: ", response.status);
+    } catch (error) {
+      if (error.name === "AbortError")
+        this.logger.error("請求超時");
+      else
+        this.logger.error(error.message);
+    } finally {
+      clearTimeout(timeout);
+      return null;
+    }
+  }
+
+  async #serviceToken() {
+    const controller = new AbortController();
+    const timeout = setTimeout(() => controller.abort(), 60000);
+
+    try {
+      const response = await fetch(`https://api.exptech.dev/api/v1/auth/service-tokens`, {
+        body: JSON.stringify({
+          note    : 'trem-lite',
+          permissions: [10_101, 10_102, 102, 20_101, 202]
+        }),
+        headers: {
+          'Authorization': `Bearer ${this.login_token}`,
+          'Content-Type': 'application/json',
+        },
+        method: 'POST',
+      });
+
+      if (response && response.ok) {
+        const ans = await response.json();
+        // this.logger.info("serviceToken: ", ans);
+        this.key = ans.token;
+        this.getconfig.user.token = this.key;
+        this.config.writeConfig(this.getconfig);
+        return this.key;
+      } else
+        this.logger.error("ServiceToken http status code: ", response.status);
     } catch (error) {
       if (error.name === "AbortError")
         this.logger.error("請求超時");
